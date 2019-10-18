d83e837c5e5dda1fcda4746ab6e13c4e87bf1d1f
Jacob Davis <jacob.marquez.davis@hotmail.com>
2019-10-18 13:33:22 -0500

    :P

--- RatStore.Data/Entities/jacobproject0Context.cs
+++ RatStore.Data/Entities/jacobproject0Context.cs
@@ -28,8 +28,7 @@ namespace RatStore.Data.Entities
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:davisjacob.database.windows.net,1433;Initial Catalog=jacob-project0;Persist Security Info=False;User ID=jacobdavis1;Password=ILikeLegos1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+
             }
         }
 
